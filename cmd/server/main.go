3b9e8d874f25a2dc7eb0247e0fac64e40489b1e1
Kalani-Kawaguchi <kkawa0442@gmail.com>
2025-06-10 19:26:09 -0500

    Update game.go and main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/Kalani-Kawaguchi/Hangman/internal/game"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	word := "Gopher"
+	hangmanGame := game.NewGame(word, 6)
+
+	fmt.Println("Welcome to Hangman!")
+	for hangmanGame.Status == game.InProgress {
+		hangmanGame.DisplayState()
+		fmt.Print("Enter a letter: ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		input := strings.TrimSpace(scanner.Text())
+		if len(input) != 1 {
+			fmt.Println("Please enter a single letter.")
+			continue
+		}
+
+		hangmanGame.Guess(rune(input[0]))
+	}
+
+	hangmanGame.DisplayState()
+	switch hangmanGame.Status {
+	case game.Won:
+		fmt.Println("You Won! The word was: ", hangmanGame.Word)
+	case game.Lost:
+		fmt.Println("Game Over! The word was: ", hangmanGame.Word)
+	}
 }
--- internal/game/game.go
+++ internal/game/game.go
@@ -2,15 +2,46 @@ package game
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 	"unicode"
 )
+
+var letters = map[rune]bool{
+	97:  false, // 'a'
+	98:  false, // 'b'
+	99:  false, // 'c'
+	100: false, // 'd'
+	101: false, // 'e'
+	102: false, // 'f'
+	103: false, // 'g'
+	104: false, // 'h'
+	105: false, // 'i'
+	106: false, // 'j'
+	107: false, // 'k'
+	108: false, // 'l'
+	109: false, // 'm'
+	110: false, // 'n'
+	111: false, // 'o'
+	112: false, // 'p'
+	113: false, // 'q'
+	114: false, // 'r'
+	115: false, // 's'
+	116: false, // 't'
+	117: false, // 'u'
+	118: false, // 'v'
+	119: false, // 'w'
+	120: false, // 'x'
+	121: false, // 'y'
+	122: false, // 'z'
+}
 
 type Game struct {
 	Word           string
 	Revealed       []rune
 	AttemptsLeft   int
-	GuessedLetters map[rune]bool
+	Letters        map[rune]bool
+	GuessedLetters []rune
 	Status         GameStatus
 }
 
@@ -22,6 +53,15 @@ const (
 	Lost
 )
 
+func ValidateLetter(r rune) bool {
+	if !unicode.IsLetter(r) || (r > unicode.MaxASCII || (r < 'A' || (r > 'Z' && r < 'a') || r > 'z')) {
+		fmt.Println("The only letters from the English alphabet are accepted.")
+		return false
+	}
+
+	return true
+}
+
 func ValidateWord(word string) bool {
 	if len(word) < 5 {
 		fmt.Println("The word must be at least 5 characters.")
@@ -29,38 +69,48 @@ func ValidateWord(word string) bool {
 	}
 
 	for _, r := range word {
-		if !unicode.IsLetter(r) || (r > unicode.MaxASCII || (r < 'A' || (r > 'Z' && r < 'a') || r > 'z')) {
-			fmt.Println("The word must only contain letters from the English alphabet")
-			return false
-		}
+		ValidateLetter(r)
 	}
 
 	return true
 }
 
 func NewGame(word string, maxAttempts int) *Game {
-	revealed := make([]rune, len(word))
-	for i := range revealed {
-		revealed[i] = '_'
-	}
-	return &Game{
-		Word:           word,
-		Revealed:       revealed,
-		AttemptsLeft:   maxAttempts,
-		GuessedLetters: make(map[rune]bool),
-		Status:         InProgress,
+	if ValidateWord(word) {
+		revealed := make([]rune, len(word))
+		for i := range revealed {
+			revealed[i] = '_'
+		}
+		return &Game{
+			Word:           strings.ToLower(word),
+			Revealed:       revealed,
+			AttemptsLeft:   maxAttempts,
+			Letters:        letters,
+			GuessedLetters: make([]rune, 0, 26),
+			Status:         InProgress,
+		}
+	} else {
+		return nil
 	}
 }
 
 func (g *Game) Guess(letter rune) bool {
+	if !ValidateLetter(letter) {
+		return false
+	}
+
 	letter = unicode.ToLower(letter)
 
-	if g.GuessedLetters[letter] {
+	if g.Letters[letter] {
 		fmt.Println("You already guessed that letter.")
 		return false
 	}
 
-	g.GuessedLetters[letter] = true
+	g.Letters[letter] = true
+	g.GuessedLetters = append(g.GuessedLetters, letter)
+	sort.Slice(g.GuessedLetters, func(i, j int) bool {
+		return g.GuessedLetters[i] < g.GuessedLetters[j]
+	})
 
 	if strings.ContainsRune(g.Word, letter) {
 		g.updateMaskedWord(letter)
@@ -95,7 +145,7 @@ func (g *Game) DisplayState() {
 	fmt.Println("\nWord:", string(g.Revealed))
 	fmt.Printf("Guessed Left: %d\n", g.AttemptsLeft)
 	fmt.Printf("Guessed letters: ")
-	for letter := range g.GuessedLetters {
+	for _, letter := range g.GuessedLetters {
 		fmt.Printf("%c ", letter)
 	}
 	fmt.Println()
