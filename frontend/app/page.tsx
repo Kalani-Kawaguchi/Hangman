10fdcf164991c6ddad838e29ecef2ee27af587cb
Kalani-Kawaguchi <kkawa0442@gmail.com>
2025-07-13 11:26:31 -0500

    Update document titles and font color

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,23 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '100vh', justifyContent: 'flex-start' }}>
-      <div style={{ height: '25vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Image src="/hangman.gif" alt="Hangman" width={0} height={0} style={{ height: 'auto', width: '75vh' }} />
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', justifyContent: 'center', minHeight: '50vh' }}>
-        <Link href="/create-lobby"><Image src="/createLobby.gif" alt='Create Lobby!' width={0} height={0} style={{ height: '10vh', width: 'auto' }}/></Link>
-        <Link href="/join-lobby"><Image src="/joinLobby.gif" alt='Join Lobby!' width={0} height={0} style={{ height: '10vh', width: 'auto' }}/></Link>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Hangman</title>
+        <meta name="description" content="Multiplayer Hangman built with Go + React (Next.js)" />
+      </Head>
+      <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '100vh', justifyContent: 'flex-start' }}>
+        <div style={{ height: '25vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Image src="/hangman.gif" alt="Hangman" width={0} height={0} style={{ height: 'auto', width: '75vh' }} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', justifyContent: 'center', minHeight: '50vh' }}>
+          <Link href="/create-lobby"><Image src="/createLobby.gif" alt='Create Lobby!' width={0} height={0} style={{ height: '10vh', width: 'auto' }}/></Link>
+          <Link href="/join-lobby"><Image src="/joinLobby.gif" alt='Join Lobby!' width={0} height={0} style={{ height: '10vh', width: 'auto' }}/></Link>
+        </div>
+      </main>
+    </>
   );
 }
